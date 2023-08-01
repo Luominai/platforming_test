fcc77e99378556c8a977724e676d3f14459f02b5
Stzhino <Stephaniez0703@gmail.com>
2023-07-31 21:32:01 -0400

    beeeeeeeeeeg boi
    Pressing S now allows you to enlarge the rat horizontally

--- Assets/Scripts/tis.cs
+++ Assets/Scripts/tis.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,13 +9,15 @@ public class tis : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-        Debug.Log("Hopefully this works, if not die.");
+        transform.Translate(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localScale += new Vector3(.05f, .01f, 0f);
+        }
     }
 }
