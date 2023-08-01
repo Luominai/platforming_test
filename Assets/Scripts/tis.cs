0c05d67fe943ef777ddc132a1e5d78d85c3cdfcd
Stzhino <Stephaniez0703@gmail.com>
2023-07-31 20:59:41 -0400

    there is nothing here

---
+++ Assets/Scripts/tis.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("Hopefully this works, if not die.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
